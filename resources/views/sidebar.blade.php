4aea7b3f61f53572c0bae4e5bbacf71068593200
Lewis White <16711731@protonmail.com>
2021-05-22 09:04:37 +0000

    remove lazy load from sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -2,7 +2,7 @@
     @foreach($photos as $photo)
                 <div class="image">
                     <a href="#{{ $photo }}">
-                        <img src="/blank.gif" data-src="{{ '/storage/thumbnail/' . $photo }}" class="img-fluid lazy"
+                        <img src="{{ '/storage/thumbnail/' . $photo }}" class="img-fluid"
                              alt="{{ $photo }}"
                              width="160" height="auto">
                     </a>
