6904d8c1ec22cdad4ebc00dfd5372276c6aa8d36
Lewis White <16711731@protonmail.com>
2021-05-27 19:53:07 +0000

    resize event handler, scroll images to center of screen

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
     if ('loading' in HTMLImageElement.prototype) {
+        sizeImages();
+    }
+
+    var links = document.querySelectorAll('.side-image');
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            var alt = links[i].children[0].alt;
+            window.location.hash = alt;
+            var scrollElem = document.querySelector('.lazy[alt="' + alt + '"]');
+            scrollElem.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+        });
+    }
+
+    if (window.location.hash.length > 2) {
+        var alt = decodeURI(window.location.hash.substring(1));
+        var scrollElem = document.querySelector('.lazy[alt="' + alt + '"]');
+        scrollElem.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    }
+
+    var doit;
+    window.onresize = function () {
+        clearTimeout(doit);
+        doit = setTimeout(resizedw, 100);
+    };
+});
+
+function resizedw() {
+    sizeImages();
+}
+
+function sizeImages() {
         var images = document.querySelectorAll('img.lazy');
         var windowHeight = window.innerHeight;
         var containerWidth = document.querySelector('.image').clientWidth;
         images.forEach(img => {
             var ratio = parseInt(img.width, 10) / parseInt(img.height, 10);
             img.style.height = containerWidth / ratio + 'px';
-            console.log(img.getAttribute('alt'), parseInt(img.style.height, 10), windowHeight);
             if (parseInt(img.style.height, 10) > windowHeight) {
-                console.log('clamping');
                 img.style.height = windowHeight + 'px';
             }
             img.src = img.dataset.src;
         });
-    } else {
-        let script = document.createElement("script");
-        script.async = true;
-        script.src =
-            "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
-        document.body.appendChild(script);
-    }
-})
-
-
+}
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar minimap">
     @foreach($photos as $photo)
                 <div class="image">
-                    <a href="#{{ $photo }}">
+                    <a href="#{{ $photo }}" class="side-image">
                         <img src="{{ '/thumbnail/' . $photo }}" class="img-fluid" loading="lazy"
                              alt="{{ $photo }}"
                              width="{{ $meta->$photo->width / 10 }}" height="{{ $meta->$photo->height / 10 }}">
